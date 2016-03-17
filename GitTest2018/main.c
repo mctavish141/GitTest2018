21e9cff9a26a0eec3cd7e733c2cb36e9f1a19d8f
mctavish141 <Serega@MacBook-Air.local>
2016-03-18 01:08:48 +0200

    Initial Commit

---
+++ GitTest2018/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GitTest2018
+//
+//  Created by Serhii Kopach on 18.03.16.
+//  Copyright (c) 2016 Serhii Kopach. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
